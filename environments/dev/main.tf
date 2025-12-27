03069e5640840d1025f7ad3a223e142f72f1a0ef
Igogo Great Oro-Oghene <edwardoronaa@gmail.com>
2025-12-27 02:41:30 -0500

    Fix Terraform backend, migrate state to S3, clean dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,9 +1,17 @@
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket  = "terraform-state-great-learning"
+    key     = "serverless/dev/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
